4cd434358d766e429af4d9fc8705a16ad1e40488
glavs <glavstroi_e@mail.ru>
2021-07-23 11:09:50 +0300

    На исправление

--- src/main/java/ru/job4j/articles/service/SimpleArticleService.java
+++ src/main/java/ru/job4j/articles/service/SimpleArticleService.java
@@ -24,10 +24,11 @@ public class SimpleArticleService implements ArticleService {
     public void generate(Store<Word> wordStore, int count, Store<Article> articleStore) {
         LOGGER.info("Геренация статей в количестве {}", count);
         var words = wordStore.findAll();
-        var articles = IntStream.iterate(0, i -> i < count, i -> i + 1)
+        //var articles =
+                IntStream.iterate(0, i -> i < count, i -> i + 1)
                 .peek(i -> LOGGER.info("Сгенерирована статья № {}", i))
                 .mapToObj((x) -> articleGenerator.generate(words))
-                .collect(Collectors.toList());
-        articles.stream().forEach(articleStore::save);
+                .forEach(articleStore::save);
+       // articles.stream().forEach(articleStore::save);
     }
 }
--- src/main/java/ru/job4j/articles/service/generator/RandomArticleGenerator.java
+++ src/main/java/ru/job4j/articles/service/generator/RandomArticleGenerator.java
@@ -1,9 +1,13 @@
 package ru.job4j.articles.service.generator;
 
+import org.hsqldb.persist.Cache;
 import ru.job4j.articles.model.Article;
 import ru.job4j.articles.model.Word;
+import ru.job4j.articles.store.WordStore;
 
+import java.lang.ref.SoftReference;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,8 +18,8 @@ public class RandomArticleGenerator implements ArticleGenerator {
         var wordsCopy = new ArrayList<>(words);
         Collections.shuffle(wordsCopy);
         var content = wordsCopy.stream()
-                .map(Word::getValue)
+                .map(Word :: getValue)
                 .collect(Collectors.joining(" "));
-        return new Article(content);
+        return new Article(content.toString());
     }
 }
